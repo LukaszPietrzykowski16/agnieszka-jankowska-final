89fab4281ed4fb8d04816965eb3c34ca6ea2206d
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-07-12 14:49:11 +0200

    form update

--- src/Kontakt.js
+++ src/Kontakt.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-
+import emailjs from '@emailjs/browser';
 
 function Kontakt() {
+ 
+  const form = useRef();
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+  
+    emailjs.sendForm('gmail', 'gmail', form.current, 'a1wPYI42oYECAyoZL')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+
+
+
   return (
       <>
 <div className='contact'>
@@ -20,7 +34,7 @@ function Kontakt() {
 
     <div className='form-contant'>
       
-    <form>
+    <form onSubmit={sendEmail} ref={form}>
     <h1> Skontaktuj się ze mną! </h1>
   <label>
     <h3> Imię: </h3>
@@ -28,9 +42,9 @@ function Kontakt() {
   </label>
   <label>
   <h3> Email: </h3>
-    <input type="text" name="name" />
+    <input type="text" name="email" />
   </label>
-  <textarea>  </textarea>
+  <textarea name="message">  </textarea>
   <input type="submit" value="Wyślij" />
 </form>
     </div>
