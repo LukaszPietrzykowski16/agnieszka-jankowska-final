2114742d2395d9580db08ec7fa0c0882e7edaddc
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-07-18 18:00:59 +0200

    gallery site has been updated

--- src/Galeria.js
+++ src/Galeria.js
@@ -58,7 +58,7 @@ export default function Galeria() {
     
         <p> 
         <h1> Reportaż ślubny </h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+        W tym dniu będę Wam towarzyszyć podczas przygotowań, ceremonii ślubu oraz wesela. Może to być tradycyjny ślub kościelny, cywilny bądź w plenerze? Niezależnie od tego gdzie chcecie złożyć przysięgę chętnie do Was dołączę i uchwycę ten moment. Zakres moich usług fotograficznych jest kompleksowy, ale również elastyczny co oznacza, że Ty decydujesz o zakresie mojej pracy oraz to co chcesz otrzymać później – odbitki, pendrive czy album. A może wszystko? Nie ma problemu </p>
       </div>
     </div>
     </Link>
@@ -70,7 +70,7 @@ export default function Galeria() {
     
     <p> 
     <h1> Plenerowe sesje ślubne </h1>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+    W tym dniu będę Wam towarzyszyć podczas przygotowań, ceremonii ślubu oraz wesela. Może to być tradycyjny ślub kościelny, cywilny bądź w plenerze? Niezależnie od tego gdzie chcecie złożyć przysięgę chętnie do Was dołączę i uchwycę ten moment. Zakres moich usług fotograficznych jest kompleksowy, ale również elastyczny co oznacza, że Ty decydujesz o zakresie mojej pracy oraz to co chcesz otrzymać później – odbitki, pendrive czy album. A może wszystko? Nie ma problemu </p>
         
   </div>
   
@@ -90,7 +90,8 @@ export default function Galeria() {
     
         <p> 
         <h1> Chrzest święty - reportaż </h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+        Chrzest święty to wyjątkowe wydarzenie w Waszej rodzinie, dlatego zróbcie sobie i swojemu dziecku piękną pamiątkę na całe życie. Wykonam dla Was reportaż z przygotowań w domu, ceremonii chrztu świętego w kościele oraz podczas uroczystości na Sali. Jeśli pogoda pozwoli możemy zrobić zdjęcia w plenerze bądź poprzestać na naturalnych fotografiach z przyjęcia rodzinnego. 
+  </p>
       </div>
     </div>
     </Link>
@@ -101,7 +102,22 @@ export default function Galeria() {
     
         <p> 
         <h1>  Chrzest święty - plener </h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+        Chrzest święty to wyjątkowe wydarzenie w Waszej rodzinie, dlatego zróbcie sobie i swojemu dziecku piękną pamiątkę na całe życie. Wykonam dla Was reportaż z przygotowań w domu, ceremonii chrztu świętego w kościele oraz podczas uroczystości na Sali. Jeśli pogoda pozwoli możemy zrobić zdjęcia w plenerze bądź poprzestać na naturalnych fotografiach z przyjęcia rodzinnego. 
+</p>
+      </div>
+      <div className='test-gallery-subsection-img'>
+        <img src={img5} alt="Chrzest święty plener"/>
+      </div>
+    </div>
+    </Link>
+    <Link to="/chrzest-plener"> 
+    <div className='test-gallery-subsection-reverse'>
+     
+      <div className='test-gallery-subsection-txt'>
+    
+        <p> 
+        <h1>  Komunia Święta  </h1>
+        Komunia święta dla dzieci to z pewnością duże przeżycie. Warto to wydarzenie sfotografować, aby dobrze zapamiętać ten dzień. Moja oferta może zawierać reportaż z przygotowań w domu, ceremonii przyjęcia komunii w kościele oraz podczas uroczystości na sali. Jestem elastyczna i możecie wybrać poszczególne elementy oferty jeśli np. w Waszym kościele jest zamówiony fotograf dla całej grupy to mogę przyjechać pod kościół zrobić rodzinne zdjęcia tylko dla Was przed uroczystością bądź tylko na salę po uroczystości. To Wy decydujecie czego potrzebujecie w tym dniu.  </p>
       </div>
       <div className='test-gallery-subsection-img'>
         <img src={img5} alt="Chrzest święty plener"/>
@@ -117,10 +133,11 @@ export default function Galeria() {
     
         <p> 
         <h1> Imprezy okolicznosciowe </h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+        Jest wiele okazji, które mogę zatrzymać w kadrze swojego obiektywu. Zapiszę w kadrach Wasze uroczystości urodzinowe czy imprezy rocznicowe. Tort, dmuchanie świeczek, zabawy i radość dzieci biegających w kółko. Nie ukrywam, że najlepiej się czuję, gdy mogę złapać takie niespodziewane momenty, które są niezaplanowane i niejednokrotnie staną się Waszymi ulubionymi momentami na zdjeciach. Na pozowane ujęcia oczywiście też znajdzie się miejsce. </p>
       </div>
     </div>
     </Link>
+
     <Link to="/ciazowe"> 
     <div className='test-gallery-subsection-reverse'>
     
@@ -128,7 +145,7 @@ export default function Galeria() {
     
         <p> 
         <h1> Ciazowe </h1>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et ex quis eros viverra rutrum. Nulla imperdiet ante sit amet ultrices iaculis. Praesent ullamcorper mi eu risus viverra ultricies. Curabitur volutpat ultrices mauris eget fringilla. Praesent mollis semper congue. </p>
+        Ciąża to niewątpliwie niezwykły czas dla każdej kobiety. Zdjęcia ciążowe to piękna pamiątka zarówno dla mamy i taty z tego wyjątkowego okresu. Podczas sesji ciążowej staram się uwiecznić wszystkie Wasze emocje, jakie towarzyszą oczekującym rodzicom na swojego maluszka. </p>
       </div>
 
       <div className='test-gallery-subsection-img'>
@@ -136,9 +153,42 @@ export default function Galeria() {
       </div>
     </div>
     </Link>
+    <Link to="/chrzest-plener"> 
+    <div className='test-gallery-subsection-reverse'>
+     
+      <div className='test-gallery-subsection-txt'>
+    
+        <p> 
+        <h1>  Sesja portretowa </h1>
+        Przyda się każdej osobie. Potrzebujesz zdjęć biznesowych na swoją stronę? A może przeszłaś/przeszedłeś zmiany w swoim życiu i chcesz pokazać światu swój nowy wizerunek? Sesję zdjęciową przeżyj jako nowe niesamowite doświadczenie. Pokażę Ci, że możesz wyglądać fantastycznie przed obiektywem, wyeksponuje Twoje zalety, których Ty nie dostrzegasz w sobie na co dzień. Poczuj radość podczas sesji zdjęciowej i uzyskaj piękne portrety, które staną się Twoimi ulubionymi. </p>
+      </div>
+      <div className='test-gallery-subsection-img'>
+        <img src={img5} alt="Chrzest święty plener"/>
+      </div>
+    </div>
+    </Link>
+
+    <Link to="/chrzest-plener"> 
+    <div className='test-gallery-subsection-reverse'>
+     
+      <div className='test-gallery-subsection-txt'>
+    
+        <p> 
+        <h1>  Osiemnastki </h1>
+        Przyda się każdej osobie. Potrzebujesz zdjęć biznesowych na swoją stronę? A może przeszłaś/przeszedłeś zmiany w swoim życiu i chcesz pokazać światu swój nowy wizerunek? Sesję zdjęciową przeżyj jako nowe niesamowite doświadczenie. Pokażę Ci, że możesz wyglądać fantastycznie przed obiektywem, wyeksponuje Twoje zalety, których Ty nie dostrzegasz w sobie na co dzień. Poczuj radość podczas sesji zdjęciowej i uzyskaj piękne portrety, które staną się Twoimi ulubionymi. </p>
+      </div>
+      <div className='test-gallery-subsection-img'>
+        <img src={img5} alt="Chrzest święty plener"/>
+      </div>
+    </div>
+    </Link>
+
+    
+    
+  
   </div>
   
-   
+    
   </>
   )
 }
