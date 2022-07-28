5da69c25a2d7e94673a8de05f8fa410904298110
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-07-28 17:33:36 +0200

    improvment of links on first page

--- src/firstPanel.js
+++ src/firstPanel.js
@@ -1,15 +1,27 @@
 import React from "react";
-
+import { Link } from 'react-router-dom';
 
 const firstPanel = () => {
     return <>
         <div className="panel">
+       
             <div className="left-panel">
-                <p className="title"> Fizjoterapia </p>
+                <Link to="/fizjoterapia">
+                    <div className="title-div">
+                        <p className="title"> Fizjoterapia </p>
+                    </div>
+                </Link>
+            </div> 
+        
+        
+           <div className="right-panel">
+                <Link to="/fotografia">
+                    <div className="title-div">
+                        <p className="title"> Fotografia </p>
+                    </div>
+                </Link>
             </div>
-            <div className="right-panel">
-                <p className="title">  Fotografia </p>
-            </div>
+        
         </div>
     
     </>
