6417bf0396b45beebe474a2c69bf088c117f99bc
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-07-28 12:18:27 +0200

    section added

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,12 @@ import Portfolio from './Portfolio';
 import Offer from './Offer';
 import Contact from './Contact';
 import Social from './Social';
-
+import Panel from './firstPanel'
 
 function Home() {
   return (
     <>
+   <Panel />
    <Content />
    <About /> 
    <Portfolio />
---
+++ src/firstPanel.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const firstPanel = () => {
+    return <>
+        <div className="panel">
+            <div className="left-panel">
+                <p className="title"> Fizjoterapia </p>
+            </div>
+            <div className="right-panel">
+                <p className="title">  Fotografia </p>
+            </div>
+        </div>
+    
+    </>
+}
+
+export default firstPanel; 
