33c68ad8af895e0b910097cb1fe6a8964e3bf320
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-08-07 22:09:27 +0200

    new front page

--- src/firstPanel.js
+++ src/firstPanel.js
@@ -1,25 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import logo1 from './images/LogoFizjo2.png';
+import logo2 from './images/logo.png';
 
 const firstPanel = () => {
     return <>
         <div className="panel">
        
-            <div className="left-panel">
-                <Link to="/fizjoterapia">
-                    <div className="title-div">
-                        <p className="title"> Fizjoterapia </p>
-                    </div>
-                </Link>
-            </div> 
-        
-        
-           <div className="right-panel">
-                <Link to="/fotografia">
-                    <div className="title-div">
-                        <p className="title"> Fotografia </p>
-                    </div>
-                </Link>
+            <div className="panel-div">
+                <img src={logo2}/>
+            </div>
+            <div className="panel-div">
+                <img src={logo2}/>
             </div>
         
         </div>
